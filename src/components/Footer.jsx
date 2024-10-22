a96ea9577eedda4d6306a902708423ade05bb596
Leon <115925556+leontran44@users.noreply.github.com>
2024-10-22 08:54:53 +0000

    add components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+	return (
+		<footer className="bg-light text-center text-lg-start">
+			<div className="container p-4">
+				<p>Connect with me on:</p>
+				<a href="https://github.com/leontran44" className="me-4">
+					GitHub
+				</a>
+				<a
+					href="https://www.linkedin.com/in/hoangqtran/"
+					className="me-4"
+				>
+					LinkedIn
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
